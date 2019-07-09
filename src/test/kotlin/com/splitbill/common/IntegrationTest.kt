aa882f416ecda7eec2a540240c03257ae733c528
Azurebird <white.camilo@gmail.com>
2019-07-08 21:18:29 -0500

    Create group integration test

--- src/test/kotlin/com/splitbill/common/IntegrationTest.kt
+++ src/test/kotlin/com/splitbill/common/IntegrationTest.kt
@@ -2,8 +2,11 @@ package com.splitbill.common
 
 import com.splitbill.AppApplication
 import com.splitbill.common.config.db.MongoConfig
+import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit.jupiter.SpringExtension
 
+@ExtendWith(SpringExtension::class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
     classes = [AppApplication::class, MongoConfig::class])
 annotation class IntegrationTest
--- src/test/kotlin/com/splitbill/group/CreateGroupIntegrationTest.kt
+++ src/test/kotlin/com/splitbill/group/CreateGroupIntegrationTest.kt
@@ -6,10 +6,15 @@ import com.splitbill.common.util.MongoUtils
 import com.splitbill.group.repository.group.GroupRepository
 import com.splitbill.profile.model.ProfileModel
 import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.web.client.TestRestTemplate
 import org.springframework.boot.test.web.client.postForEntity
+import org.springframework.http.HttpEntity
 import org.springframework.http.HttpHeaders
+import org.springframework.util.LinkedMultiValueMap
+import org.springframework.util.MultiValueMap
+
 
 // TODO this integration test
 @IntegrationTest
@@ -18,24 +23,37 @@ class CreateGroupIntegrationTest(
     @Autowired val restTemplate: TestRestTemplate
 ) {
 
+    val email = "mail@mail.com"
+    val password = "pass"
+
     @BeforeAll
     fun clean() {
         MongoUtils.cleanDB()
     }
 
+    @Test
     fun `Created group`() {
-        val email = "mail@mail.com"
-        val request = ProfileModel(email, "pass")
-        restTemplate.postForEntity<ProfileModel>("/profile/", request).body!!
-        val loginToken = restTemplate.postForEntity<TokenResponseEntity>("/login/", mapOf("email" to email, "password" to "pass")).body!!
 
+        val loginToken = doLogin(createProfile())
 
         val headers = HttpHeaders()
         headers.setBearerAuth(loginToken.token)
-        //val entity = HttpEntity("body", headers)
+        val map = LinkedMultiValueMap<String, String>()
+        map.add("name", "familia")
+        val request = HttpEntity(map, headers)
+        restTemplate.postForEntity<Any>("/group/", request)
+    }
 
+    private fun createProfile() : ProfileModel {
+        return restTemplate.postForEntity<ProfileModel>("/profile/", ProfileModel(email, "pass")).body!!
+    }
 
-        restTemplate.postForEntity<Any>("/group/", headers, mapOf("name" to "familia")).body!!
+    private fun doLogin(profile: ProfileModel) : TokenResponseEntity {
+        val map = LinkedMultiValueMap<String, String>()
+        map.add("email", profile.email)
+        map.add("password", password)
 
+        val request = HttpEntity<MultiValueMap<String, String>>(map, HttpHeaders())
+        return restTemplate.postForEntity<TokenResponseEntity>("/login/", request).body!!
     }
 }
